202de1d36e0b69288a3bea6e297399c56908a8f1
Romulo Enrique Troncoso Pacheco <47163593+romulotroncosop@users.noreply.github.com>
2021-10-26 22:28:31 -0500

    first commit
    first commit

---
+++ UCRepo/Areas/Repositorio/Controllers/RepositorioController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace UCRepo.Areas.Repositorio.Controllers
+{
+    //Load area
+    [Area("Repositorio")]
+    public class RepositorioController : Controller
+    {
+        public IActionResult Repositorio()
+        {
+            return View();
+        }
+    }
+}
